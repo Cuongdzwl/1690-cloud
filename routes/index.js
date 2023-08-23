6cc122eb71fd2ee0d6ce35f66193c871b083a921
Cuong Nguyen <91537591+Cuongdzwl@users.noreply.github.com>
2023-08-24 00:37:54 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    var products = await Product.find().sort({createdAt : 1}).limit(6);
+    var products = await Product.find().sort({createdAt : -1}).limit(6);
     var number = await Subscriber.countDocuments();
   res.render("index", {title : "ATN SHOP", products : products, number : number});
 });
